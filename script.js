d9e47d6854910f4a60eabdd2616bb2588b5a6fd0
Agustina Fleitas <141256804+agustinafleitas@users.noreply.github.com>
2025-08-04 18:12:31 -0300

    Ajustes a la actividad

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //Almacenar en una variable el contenedor y el botón
 
-... .addEventListener('click', () => {
-    contenedorPerrito.textContent = 'Cargando perrito...';
+/*COMPLETAR*/.addEventListener('click', () => { //¿A quien le agregamos el evento?¿Donde debemos hacer click?
+   /*COMPLETAR*/.textContent = 'Cargando perrito...';
 
     // Completa la URL correcta de la API dentro del fetch
-    fetch(/* Aquí va la URL de la API, que esta en el readme.md */)
+    fetch(/*COMPLETAR*/)
+
     // Convierte la respuesta a JSON
-        .then(response => /* Completar aquí para convertir a JSON */)
+        .then(response => /*COMPLETAR*/)
+
     // Usa los datos para mostrar la imagen
         .then(data => {
-        // Usa data.message para obtener la URL de la imagen
-            contenedorPerrito.innerHTML = `
-            <img src="${/* Completar con la URL de la imagen */}" alt="Imagen de perrito" />
+        // Usa obtener la URL de la imagen y mostrarla en el contenedor. ¿Qué etiqueta HTML necesitamos para cargar la imagen? ¿Qué información de la API vamos a cargar?
+            /*COMPLETAR*/.innerHTML = `
+            /*COMPLETAR*/
         `;
         console.log(data);
     })
+
     // En caso de error muestra un mensaje y el error en consola
     .catch(error => {
         console.error('Error al obtener imagen:', error);
-        contenedorPerrito.textContent = 'Hubo un problema al cargar la imagen.';
+        /*COMPLETAR*/.textContent = ' COMPLETAR ';
     });
 });
